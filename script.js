2d260a5afa526c9c0a4bafa5fc770fb0aabfaab8
Jacquin-noe <105502405+Jacquin-noe@users.noreply.github.com>
2022-05-15 19:03:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("May the 4th be with you!")
+function bigImg(x) {
+  x.style.height = "200px";
+  x.style.width = "400px";
+}
+
+function normalImg(x) {
+  x.style.height = "100px";
+  x.style.width = "200px";
+}
